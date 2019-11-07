ff32ea5b73133cedc73249524abb464f4bc4d87d
Ruslan3584 <37312893+Ruslan3584@users.noreply.github.com>
2019-11-07 20:33:57 +0200

    added new functionality
    now we can recognize sum of 2 digits

--- lab3/lab_3.py
+++ lab3/lab_3.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[61]:
 
 
 import numpy as np
 
 
-# In[2]:
+# In[62]:
 
 
 zero = np.array([[1, 1, 1],
@@ -72,20 +72,20 @@ nine = np.array([[1, 1, 1],
                  [0, 0, 1]])
 
 
-# In[3]:
+# In[63]:
 
 
 Y = [zero, one, two, three, four, five, six, seven, eight, nine] # еталонні зображення
 
 
-# In[4]:
+# In[64]:
 
 
 n = Y[0].shape[0]
 m = Y[0].shape[1]
 
 
-# In[5]:
+# In[65]:
 
 
 def upscale(orig_array,dig_1, dig_2):
@@ -102,69 +102,134 @@ def upscale(orig_array,dig_1, dig_2):
     return d
 
 
-# In[6]:
-
-
-upscale(Y[1],2,2)
-
-
-# In[7]:
+# In[66]:
 
 
 histogram = np.random.randint(0,100,10)   # change value of seed if you want to get new histogram
 histogram = histogram/np.sum(histogram)  # normilizing of histogram
+histogram = [0]*10
+histogram[1] =1
 histogram
 
 
-# In[8]:
+# In[74]:
 
 
-t = 3  # 3 цифри
+def task(Y, t, p):
+    r = np.random.randint(0,9,t)
+    x_etalone = Y[r[0]]
+    for i in range(1,t):
+        x_etalone = np.concatenate((x_etalone,Y[r[i]]),axis=1)
+    n = Y[0].shape[0]
+    m = Y[0].shape[1]
+    ksi = np.random.binomial(size=n*m*t, n=1, p=p).reshape((n,m*t))
+    print(r)
+    return x_etalone^ksi
+x = task(Y,3,0.8)
 
 
-# In[12]:
+# In[68]:
 
 
-r = np.random.randint(0,9,t)
-x_etalone = Y[np.random.randint(0,9)]
-for i in range(t-1):
-    x_etalone = np.concatenate((x_etalone,Y[np.random.randint(0,9)]),axis=1)
+x
 
 
-# In[13]:
+# In[69]:
 
 
-x_etalone
+def recognition(task,noise_level, numbers):        
+    result = []
+    for i in numbers:
+        d = []
+        noise = i^task          
+        
+        noise = noise.reshape(1, noise.shape[0]*noise.shape[1])
+        
+        for j in range(0, noise.shape[0]*noise.shape[1]):           
+            if noise[0][j] == 0:
+                d.append(1-noise_level)
+            else:
+                d.append(noise_level)
+
+        result.append(np.prod(d))              
+    return result
 
 
-# In[14]:
+# In[70]:
 
 
-Y[np.random.randint(0,9)]
+d = np.hsplit(x, 3)
+d
 
 
-# In[15]:
+# In[ ]:
 
 
-ksi = np.random.binomial(size=n*m*t, n=1, p=0.2).reshape((n,m*t))
 
 
-# In[16]:
+
+# In[71]:
 
 
-ksi
+x1 =d[0]
+recognition(x1,0.2, Y)*histogram
 
 
-# In[17]:
+# In[ ]:
 
 
-x = x_etalone^ksi
+
+
+
+# In[72]:
+
+
+x12 = np.concatenate((d[0],d[1]),axis=1)
+
+
+# In[75]:
+
+
+res = []
+for i in range(0,19):
+    k2 = []
+    for j in range (0,10):
+        
+        if j>=i-9 and j<=i:
+            
+            x1 = d[0]
+            x2 = d[1]
+            p22 = recognition(x2,0.8,Y)[j]*histogram[j]
+            p12 = recognition(x1,0.8,Y)[i-j]*histogram[i-j]
+            p = p12*p22
+            #print(p)
+            k2.append(p)
+    res.append(np.sum(k2))
+(res)
 
 
 # In[18]:
 
 
-x
+k2
+
+
+# In[16]:
+
+
+recognition(x2,0.2,Y)[2]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
