6b5a4e8e44a9fe9788542d4fb8714d1912315f58
Maksym Shylo <33497690+maksymshylo@users.noreply.github.com>
2019-12-25 15:47:24 +0200

    for running tests

---
+++ lab6/lab_6.py
@@ -0,0 +1,9 @@
+from functions import *
+
+a,b,r = parameters()
+data = get_data(20)
+s_data = split_data(data,a,b,r)
+
+result = perceptron(s_data)
+print('result_parameters',result)
+print('original_parameters',a,b,r)
